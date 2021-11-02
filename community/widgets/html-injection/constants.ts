66620d52dd4b401de6b9cba62e5f31b6e282c096
Jan Mach <janmach1@microsoft.com>
2021-11-02 11:51:20 +0100

    HTML widget prepared as example

---
+++ community/widgets/html-injection/constants.ts
@@ -0,0 +1,6 @@
+export const widgetName = "html-injection";
+export const widgetDisplayName = "HTML injection";
+export const widgetCategory = "Community";
+export const widgetSelector = "widget";
+export const widgetRuntimeSelector = "widget-runtime";
+export const widgetEditorSelector = "widget-editor";
