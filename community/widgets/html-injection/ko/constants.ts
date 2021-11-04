4d6ac8c1dbb344f36827c1d95ebe2ca1e4d5280c
Jan Mach <janmach1@microsoft.com>
2021-11-04 17:00:43 +0100

    html injection widget 1 base implementation

---
+++ community/widgets/html-injection/ko/constants.ts
@@ -0,0 +1,13 @@
+export const htmlCodeInitial = 
+`<html>
+<head>
+    <style>
+        body {
+            margin: 0;
+        }
+    </style>
+</head>
+<body>
+    <span>Wite your HTML here</span>
+</body>
+</html>`;
